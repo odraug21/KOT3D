a1071e2d9058371e86e9a29812bba697740c15bc
CARLO JOSE GUARDO PADILLA <cguardo21@gmail.com>
2026-02-04 18:59:42 -0300

    Actualizacion AppRoutes.jsx

--- src/Routes/AppRouter.jsx
+++ src/Routes/AppRouter.jsx
@@ -3,21 +3,22 @@ import Home from "../views/public/Home";
 import Login from "../views/public/Login";
 import Register from "../views/public/Register";
 
-import Store from "../views/private/Store";          // puedes moverlo a public si quieres, pero no es obligatorio
-import PostDetail from "../views/private/PostDetail"; // idem
+import Store from "../views/private/Store";
+import PostDetail from "../views/private/PostDetail";
 
 import CreatePost from "../views/private/CreatePost";
 import Profile from "../views/private/Profile";
 import Favorites from "../views/private/Favorites";
 import MyPosts from "../views/private/MyPosts";
 import EditPost from "../views/private/Editpost";
+
 import AdminImagenes from "../pages/AdminImagenes";
+import AdminUsers from "../pages/Admin/AdminUsers";
 
 import CartPage from "../pages/Cart/Cart";
 import UploadProof from "../pages/Orders/UploadProof";
 
 import ProtectedRoute from "./ProtectedRoute";
-import AdminUsers from "../pages/Admin/AdminUsers";
 
 export default function AppRouter() {
   return (
@@ -39,7 +40,10 @@ export default function AppRouter() {
         <Route path="/favorites" element={<Favorites />} />
         <Route path="/my-posts" element={<MyPosts />} />
         <Route path="/posts/:id/edit" element={<EditPost />} />
+
+        {/* ✅ Admin (por ahora: solo logeado; lo hacemos solo-admin si quieres) */}
         <Route path="/admin/imagenes" element={<AdminImagenes />} />
+        <Route path="/admin/users" element={<AdminUsers />} />
 
         {/* ✅ Subir comprobante (solo logeado) */}
         <Route path="/orders/:id/proof" element={<UploadProof />} />
